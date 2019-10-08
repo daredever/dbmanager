fa4080f04f54e686ada7a8feb4750620c7e26795
Roman <daredever@gmail.com>
2019-10-08 08:59:38 +0300

    add swagger

--- dbmanager.API/Startup.cs
+++ dbmanager.API/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace dbmanager.API
 {
@@ -35,6 +35,12 @@ namespace dbmanager.API
 
             services.AddDistributedMemoryCache();
             services.AddSession();
+
+            // Register the Swagger generator,
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +54,16 @@ namespace dbmanager.API
             app.UseSession();
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.)
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
 
             app.UseRouting();
 
