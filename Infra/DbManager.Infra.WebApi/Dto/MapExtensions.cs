8d2ad468f7a1ea10029f6a2b1899ad4777c97fa2
Roman Shcherbakov <daredever@gmail.com>
2020-04-29 08:54:05 +0300

    fix

--- Infra/DbManager.Infra.WebApi/Dto/MapExtensions.cs
+++ Infra/DbManager.Infra.WebApi/Dto/MapExtensions.cs
@@ -1,12 +1,19 @@
-﻿using DbManager.Domain.Models;
+﻿using System;
+using DbManager.Domain.Models;
 using DbManager.Domain.Models.DefaultImpl;
 
 namespace DbManager.Infra.WebApi.Dto
 {
     internal static class MapExtensions
     {
-        public static ColumnDto Map(this IColumn model) =>
-            new ColumnDto
+        public static ColumnDto Map(this IColumn model)
+        {
+            if (model is null)
+            {
+                return null;
+            }
+
+            return new ColumnDto
             {
                 Catalog = model.Catalog,
                 Schema = model.Schema,
@@ -15,9 +22,16 @@ namespace DbManager.Infra.WebApi.Dto
                 IsNullable = model.IsNullable,
                 CharactersMaxLength = model.CharactersMaxLength
             };
+        }
 
-        public static IColumn Map(this ColumnDto dto) =>
-            new Column
+        public static IColumn Map(this ColumnDto dto)
+        {
+            if (dto is null)
+            {
+                return null;
+            }
+
+            return new Column
             {
                 Catalog = dto.Catalog,
                 Schema = dto.Schema,
@@ -26,33 +40,62 @@ namespace DbManager.Infra.WebApi.Dto
                 IsNullable = dto.IsNullable,
                 CharactersMaxLength = dto.CharactersMaxLength
             };
+        }
 
-        public static TableDto Map(this ITable model) =>
-            new TableDto
+        public static TableDto Map(this ITable model)
+        {
+            if (model is null)
+            {
+                return null;
+            }
+
+            return new TableDto
             {
                 Catalog = model.Catalog,
                 Schema = model.Schema,
                 Name = model.Name
             };
+        }
 
-        public static ITable Map(this TableDto dto) =>
-            new Table
+        public static ITable Map(this TableDto dto)
+        {
+            if (dto is null)
+            {
+                return null;
+            }
+
+            return new Table
             {
                 Catalog = dto.Catalog,
                 Schema = dto.Schema,
                 Name = dto.Name
             };
+        }
 
-        public static CatalogDto Map(this ICatalog model) =>
-            new CatalogDto
+        public static CatalogDto Map(this ICatalog model)
+        {
+            if (model is null)
+            {
+                return null;
+            }
+
+            return new CatalogDto
             {
                 Name = model.Name
             };
+        }
 
-        public static ICatalog Map(this CatalogDto dto) =>
-            new Catalog
+        public static ICatalog Map(this CatalogDto dto)
+        {
+            if (dto is null)
+            {
+                return null;
+            }
+
+            return new Catalog
             {
                 Name = dto.Name
             };
+        }
     }
 }
